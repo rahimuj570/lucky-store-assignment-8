4fbaf6b26400aa87cc8eb2bee1480b16a6d40282
rahimuj570 <rahimuj570@gmail.com>
2022-03-26 15:37:23 +0600

    successfully data fetched

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SelectedBook from "../Cart/SelectedBook";
 import SingleBook from "../SingleBook/SingleBook";
 
 const Main = () => {
+  const [book, setBook] = useState([]);
+  useEffect(() => {
+    fetch("FakeData/fakeApi.json")
+      .then((res) => res.json())
+      .then((data) => setBook(data));
+  }, []);
   return (
     <div className="grid grid-cols-6">
       <div className="col-span-4">
-        <SingleBook></SingleBook>
+        <div className="grid grid-cols-1 sm:grid-cols-3 bg-scale-100 gap-5 m-5">
+          {book.map((data) => (
+            <SingleBook key={data.id} data={data}></SingleBook>
+          ))}
+        </div>
       </div>
-      <div className="shadow col-span-2 bg-yellow-100">
+      <div className=" shadow col-span-2 bg-yellow-100">
         <h1 className="bg-yellow-200 text-center p-3 p-1 font-semibold text-xl">
           Selected Books
         </h1>
