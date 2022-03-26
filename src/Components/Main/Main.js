a966977f25a67766c2f950241e35caf035e95a82
rahimuj570 <rahimuj570@gmail.com>
2022-03-26 16:02:30 +0600

    dynamically show data from json

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -9,16 +9,17 @@ const Main = () => {
       .then((res) => res.json())
       .then((data) => setBook(data));
   }, []);
+  console.log(book);
   return (
     <div className="grid grid-cols-6">
-      <div className="col-span-4">
+      <div className="col-span-3 sm:col-span-4">
         <div className="grid grid-cols-1 sm:grid-cols-3 bg-scale-100 gap-5 m-5">
           {book.map((data) => (
             <SingleBook key={data.id} data={data}></SingleBook>
           ))}
         </div>
       </div>
-      <div className=" shadow col-span-2 bg-yellow-100">
+      <div className=" shadow col-span-3 sm:col-span-2 bg-yellow-100">
         <h1 className="bg-yellow-200 text-center p-3 p-1 font-semibold text-xl">
           Selected Books
         </h1>
--- src/Components/SingleBook/SingleBook.js
+++ src/Components/SingleBook/SingleBook.js
@@ -1,20 +1,21 @@
 import React from "react";
 
-const SingleBook = () => {
+const SingleBook = ({ data }) => {
   return (
-    <div className=" shadow">
-      <img
-        className="p-2 w-98 h-98 object-cover"
-        src="chrome://branding/content/about-logo.png"
-        alt=""
-      />
-      <div className="p-1 bg-slate-100">
-        <p className="font-semibold">Name: Books</p>
-        <p className="font-semibold">Price: 20</p>
+    <div className=" shadow relative">
+      <div className="mw-80 h-60 sm:h-80">
+        <img className="w-full h-full object-cover" src={data.picture} alt="" />
       </div>
-      <button className="font-bold w-full btn bg-yellow-300 p-1">
-        Add To Cart
-      </button>
+      <div className="bg-slate-100">
+        <div className="p-1 pb-5">
+          <p className="font-semibold">Name: {data.name}</p>
+          <p className="font-semibold">Price: {data.price}</p>
+        </div>
+        <button></button>
+        <button className="absolute bottom-0 font-bold w-full btn bg-yellow-300 p-1">
+          Add To Cart
+        </button>
+      </div>
     </div>
   );
 };
